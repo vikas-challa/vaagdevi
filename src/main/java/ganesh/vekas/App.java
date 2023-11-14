f0ff3a2755538f264fd737862518d7247a48be1e
vikas-challa <149367220+vikas-challa@users.noreply.github.com>
2023-11-14 17:42:41 +0530

    Add files via upload

---
+++ src/main/java/ganesh/vekas/App.java
@@ -0,0 +1,13 @@
+package ganesh.vekas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
